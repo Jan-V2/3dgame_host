bd11ed2c8d859d9aec2c3d7fc2f269fe92a148fa
johnvanderholt <jdqwertqwert@gmail.com>
2018-10-30 11:31:40 +0100

    levels opgeschoont

--- 3D Game/AmazonSimulator VS/Levels/AllLevels.cs
+++ 3D Game/AmazonSimulator VS/Levels/AllLevels.cs
@@ -12,29 +12,5 @@ namespace Levels
             {'l','s','v','e','l'},
             {'l','l','l','l','l'}
         };
-
-        public List<Level> level = new List<Level>();
-
-        /// <summary>
-        /// Checks if the list which is used to send the levels to the client is empty or
-        /// if there are some variables already in it from a previous level
-        /// and resets it to make room for the new level
-        /// </summary>
-        public void CheckLevelList()
-        {
-            if (level != null)
-            {
-                level = new List<Level>();
-            }
-        }
-        /// <summary>
-        /// Create level one and adds it to a list
-        /// </summary>
-        /// <returns>level</returns>
-
-        public void LevelFive()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
