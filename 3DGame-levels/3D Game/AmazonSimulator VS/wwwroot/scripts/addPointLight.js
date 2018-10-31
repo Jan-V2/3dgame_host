d7648371d78ab31e5c9923d630ead2d630b9353c
verburg95 <davidverburg@hotmail.nl>
2018-10-31 11:16:34 +0100

    level7

---
+++ 3DGame-levels/3D Game/AmazonSimulator VS/wwwroot/scripts/addPointLight.js
@@ -0,0 +1,12 @@
+﻿class addPointLight extends THREE.Group {
+
+    constructor(mesh, color, posx, posy, posz) {
+        super();
+        var light = new THREE.PointLight(color);
+        mesh.add(light);
+        mesh.add(light.position);
+        light.position.set(posx, posy, posz);
+        light.intensity = 2;
+        light.distance = 1;
+    }
+}
